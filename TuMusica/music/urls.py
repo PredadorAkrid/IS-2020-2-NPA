d213f224616d9fb1803389debfe42b7741bf55e1
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-03-03 01:10:04 -0600

    cuarto commit

---
+++ TuMusica/music/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from django.conf.urls import url,include
+from music.views import *
+urlpatterns = [
+    path('',index),
+]
--- TuMusica/music/views.py
+++ TuMusica/music/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'GET':
+        return render(request,'index.html')
+    elif request.method == 'POST':
+        print("entra a index post")
+        return HttpResponseForbidden()
